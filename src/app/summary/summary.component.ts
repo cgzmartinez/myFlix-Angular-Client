ae18e283c02552ede7b6b6a20d9a9a27c9db247f
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-11-05 15:27:37 -0400

    Created Summary Component

---
+++ src/app/summary/summary.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss']
+})
+export class SummaryComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Title: string,
+      Description: string,
+    }
+  ) { }
+
+
+  ngOnInit(): void {
+  }
+
+}
