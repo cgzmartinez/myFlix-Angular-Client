941c2d0457b8cb42ad5e1f3b5b7cfdd56e197d86
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-11-05 15:27:18 -0400

    Created Toolbar

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor(
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  goToMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  goToProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  logOut(): void {
+    localStorage.clear();
+    this.router.navigate(['welcome']);
+  }
+
+
+}
